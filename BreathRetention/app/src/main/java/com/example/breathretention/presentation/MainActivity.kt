1068f51d3af22ca07c47dadab1c54443b0f84a87
AnastasiaBlinova <nastya.com107@gmail.com>
2023-04-05 19:02:10 +0300

    Test exercises 3.0

--- BreathRetention/app/src/main/java/com/example/breathretention/presentation/MainActivity.kt
+++ BreathRetention/app/src/main/java/com/example/breathretention/presentation/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.breathretention.presentation
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -8,19 +9,30 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.webkit.CookieManager
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import com.example.breathretention.R
 import com.example.breathretention.databinding.ActivityMainBinding
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var webView: WebView
+    val remoteConfig = FirebaseRemoteConfig.getInstance().getString("url")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.webView.WebViewTransport()
 
         setSupportActionBar(binding.toolbar)
 
@@ -40,5 +52,15 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        webView = findViewById(R.id.web_view)
+        webView.setWebViewClient(WebViewClient())
+        if (remoteConfig == null)
+            webView.loadUrl("https://www.google.ru/")
+        val cookieManager = CookieManager.getInstance()
+        cookieManager.setAcceptCookie(true)
     }
 }
