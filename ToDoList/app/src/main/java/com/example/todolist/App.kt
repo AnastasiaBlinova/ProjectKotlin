491ecb7de5c08fc9110def24d4ad6976891042df
AnastasiaBlinova <nastya.com107@gmail.com>
2023-04-10 20:16:35 +0300

    Test exercises

---
+++ ToDoList/app/src/main/java/com/example/todolist/App.kt
@@ -0,0 +1,19 @@
+package com.example.todolist
+
+import android.app.Application
+import androidx.room.Room
+
+/*class App: Application() {
+
+    lateinit var db: AppDatabase
+
+    override fun onCreate() {
+        super.onCreate()
+        db = Room.databaseBuilder(       //databaseBuilder передаем :
+            applicationContext,                 // контекст
+            AppDatabase::class.java,             // класс датабэйс
+            "bd"
+        )
+            .build()
+    }
+}*/
