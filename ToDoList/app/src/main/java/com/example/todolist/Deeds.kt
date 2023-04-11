48adc7102430d58df2d2c34f060bd1e5a78ee36e
AnastasiaBlinova <nastya.com107@gmail.com>
2023-04-11 13:37:09 +0300

    Test exercises

---
+++ ToDoList/app/src/main/java/com/example/todolist/Deeds.kt
@@ -0,0 +1,21 @@
+package com.example.todolist
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.sql.Timestamp
+
+/*@Entity(tableName = "list")
+data class Deeds(
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    val id: Int,
+    @ColumnInfo(name = "date_start")
+    val dateStart: Timestamp,
+    @ColumnInfo(name = "date_finish")
+    val dateFinish: Timestamp,
+    @ColumnInfo(name = "name")
+    val name: String?,
+    @ColumnInfo(name = "description")
+    val description: String?
+)*/
