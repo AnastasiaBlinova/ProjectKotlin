7d262b2d6883489091ef356d7223d290e3d16650
AnastasiaBlinova <nastya.com107@gmail.com>
2023-04-13 11:37:40 +0300

    Test exercises

--- ToDoList/app/src/main/java/com/example/todolist/DealDao.kt
+++ ToDoList/app/src/main/java/com/example/todolist/DealDao.kt
@@ -2,9 +2,18 @@ package com.example.todolist
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Transaction
+import kotlinx.coroutines.flow.Flow
 
-@Dao
+/*@Dao
 interface DealDao {
+    @Transaction
+    @Query("SELECT * FROM list")
+    fun getAllDeeds(): Flow<List<Deeds>>  // для получения всего списка;  Flow позволяет получать данные об обновлениях
 
-   // @Query("SELECT * FROM list WHERE ")
-}
+    @Query("delete from list ")
+    suspend fun deleteAll()      // для удаления одного  (words: Words)
+
+    @Query("SELECT * FROM list WHERE date LIKE:date")
+    fun getDeeds(date: String): Flow<List<Deeds>>
+}*/
