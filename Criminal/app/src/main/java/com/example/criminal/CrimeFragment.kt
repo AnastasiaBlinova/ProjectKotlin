fe2b731b2a9efade715818a0804e348dac2f6967
AnastasiaBlinova <nastya.com107@gmail.com>
2023-06-10 12:38:45 +0300

    crime

--- Criminal/app/src/main/java/com/example/criminal/CrimeFragment.kt
+++ Criminal/app/src/main/java/com/example/criminal/CrimeFragment.kt
@@ -1,4 +1,13 @@
 package com.example.criminal
 
-class CrimeFragment {
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class CrimeFragment: Fragment() {
+    private lateinit var crime: CrimeData
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = CrimeData()
+    }
 }
