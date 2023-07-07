0f16c46638cc09f8714f94a70e108db12c48beb0
AnastasiaBlinova <nastya.com107@gmail.com>
2023-07-07 16:51:38 +0300

    commit

---
+++ MovieListExample/app/src/main/java/com/example/movielistexample/models/Movie.kt
@@ -0,0 +1,18 @@
+package com.example.movielistexample.models
+
+data class Movie(
+    val kinipoiskId: Int,
+    val nameRu: String,
+    val posterUri: String,
+    val posterUriPreView: String,
+    val genres: List<Genre>,
+    val premierRu: String,
+    val countries: List<Country>
+)
+
+data class Genre(
+    val genre:String
+)
+data class Country(
+    val country: String
+)
