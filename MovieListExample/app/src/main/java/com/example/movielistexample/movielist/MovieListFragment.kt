ff2b8e3547a119e0494975fc8f77d3d7e0d586f1
AnastasiaBlinova <nastya.com107@gmail.com>
2023-07-07 20:48:20 +0300

    commit

---
+++ MovieListExample/app/src/main/java/com/example/movielistexample/movielist/MovieListFragment.kt
@@ -0,0 +1,44 @@
+package com.example.movielistexample.movielist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.example.movielistexample.databinding.FragmentMovieListBinding
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+
+class MovieListFragment : Fragment() {
+    private var _binding: FragmentMovieListBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: MovieListViewModel by viewModels()
+
+    private val movieAdapter = MovieAdapter()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMovieListBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.recycler.adapter = movieAdapter
+
+        viewModel.movies.onEach {
+            movieAdapter.setData(it)
+        }.launchIn(viewLifecycleOwner.lifecycleScope)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
