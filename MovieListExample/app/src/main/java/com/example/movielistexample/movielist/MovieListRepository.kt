b8d76efb57b8f6dc313d1e550d4c8fcd31c48200
AnastasiaBlinova <nastya.com107@gmail.com>
2023-07-09 10:11:06 +0300

    commit

--- MovieListExample/app/src/main/java/com/example/movielistexample/movielist/MovieListRepository.kt
+++ MovieListExample/app/src/main/java/com/example/movielistexample/movielist/MovieListRepository.kt
@@ -2,9 +2,11 @@ package com.example.movielistexample.movielist
 
 import com.example.movielistexample.api.retrofit
 import com.example.movielistexample.models.Movie
+import kotlinx.coroutines.delay
 
 class MovieListRepository {                                    //вызывается viewModel
     suspend fun getPremieres(year: Int, month: String): List<Movie>{
+        delay(1000)
         return retrofit.movies(year,month).items
     }
 }
