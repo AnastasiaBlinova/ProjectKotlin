cf6d4da50b967ae21bac567008acb1ba74d9b992
AnastasiaBlinova <nastya.com107@gmail.com>
2023-07-11 22:30:54 +0300

    commit

--- MovieListExample/app/src/main/java/com/example/movielistexample/movielist/MovieListViewModel.kt
+++ MovieListExample/app/src/main/java/com/example/movielistexample/movielist/MovieListViewModel.kt
@@ -3,7 +3,12 @@ package com.example.movielistexample.movielist
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.example.movielistexample.models.Movie
+import com.example.movielistexample.pagedmovielist.MoviePagingSource
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
@@ -18,35 +23,40 @@ class MovieListViewModel private constructor(
 
     val filterEnabled= MutableStateFlow(false)
 
-    private val _movies = MutableStateFlow<List<Movie>>(emptyList())
-    val movies: StateFlow<List<Movie>> = combine(_movies, filterEnabled){ movies, filterEnabled ->
-        if (filterEnabled)
-            movies.filter { movie ->
-                movie.countries.any { it.country == "Россия" }
-            }
-        else movies
-    }.stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.Eagerly,
-        initialValue = _movies.value
-    )
+    val pagedMovies: Flow<PagingData<Movie>> = Pager(
+        config = PagingConfig(pageSize = 10),
+        pagingSourceFactory = {MoviePagingSource()}
+    ).flow.cachedIn(viewModelScope)
 
-    init{                           //при создании viewModel вызывается функция loadPremieres()
-        loadPremieres()
-    }
+//    private val _movies = MutableStateFlow<List<Movie>>(emptyList())
+//    val movies: StateFlow<List<Movie>> = combine(_movies, filterEnabled){ movies, filterEnabled ->
+//        if (filterEnabled)
+//            movies.filter { movie ->
+//                movie.countries.any { it.country == "Россия" }
+//            }
+//        else movies
+//    }.stateIn(
+//        scope = viewModelScope,
+//        started = SharingStarted.Eagerly,
+//        initialValue = _movies.value
+//    )
 
-    private fun loadPremieres(){
-        viewModelScope.launch ( Dispatchers.IO){
-            kotlin.runCatching {                            // припомощи ретрофита загружается список премьер за январь 2022
-                _isLoading.value = true  //как только начинается загрузка в StateFlow записывается ТРУ
-                repository.getPremieres(2022, "JANUARY")
-            }.fold(
-                onSuccess = {_movies.value = it},    // после успешной загрузки данных список помещается в StateFlow, на который будет подписан фрагмент
-                onFailure = { Log.d("MovieListViewModel", it.message ?: "")}
-            )
-            _isLoading.value = false // после окончания загрузки ФОЛС
-        }
-    }
+//    init{                           //при создании viewModel вызывается функция loadPremieres()
+//        loadPremieres()
+//    }
+//
+//    private fun loadPremieres(){
+//        viewModelScope.launch ( Dispatchers.IO){
+//            kotlin.runCatching {                            // припомощи ретрофита загружается список премьер за январь 2022
+//                _isLoading.value = true  //как только начинается загрузка в StateFlow записывается ТРУ
+//                repository.getPremieres(2022, "JANUARY")
+//            }.fold(
+//                onSuccess = {_movies.value = it},    // после успешной загрузки данных список помещается в StateFlow, на который будет подписан фрагмент
+//                onFailure = { Log.d("MovieListViewModel", it.message ?: "")}
+//            )
+//            _isLoading.value = false // после окончания загрузки ФОЛС
+//        }
+//    }
 }
 
 
