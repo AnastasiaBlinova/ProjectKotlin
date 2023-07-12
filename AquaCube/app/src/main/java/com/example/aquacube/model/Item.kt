e678be08f7f779428a52a8d256b4ee056537fd01
AnastasiaBlinova <nastya.com107@gmail.com>
2023-07-12 09:12:47 +0300

    application for Aqua cube

--- AquaCube/app/src/main/java/com/example/aquacube/model/Item.kt
+++ AquaCube/app/src/main/java/com/example/aquacube/model/Item.kt
@@ -1,4 +1,19 @@
 package com.example.aquacube.model
 
 class Item {
+    var id: String? = null
+    var time: String? = null
+    var name: String? = null
+    var count: Int? = null
+
+    constructor(){}
+    constructor(
+        time: String?,
+        name: String?,
+        count: Int
+    ){
+        this.time = time
+        this. name =  name
+        this.count = count
+    }
 }
