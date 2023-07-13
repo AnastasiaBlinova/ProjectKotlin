0f277200b3e7c9f40e5d4d30206282aab1d89513
AnastasiaBlinova <nastya.com107@gmail.com>
2023-07-13 12:29:48 +0300

    application for Aqua cube

--- AquaCube/app/src/main/java/com/example/aquacube/adapter/UserAdapter.kt
+++ AquaCube/app/src/main/java/com/example/aquacube/adapter/UserAdapter.kt
@@ -1,4 +1,45 @@
 package com.example.aquacube.adapter
 
-class UserAdapter {
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.aquacube.ChatActivity
+import com.example.aquacube.R
+import com.example.aquacube.databinding.ItemProfileBinding
+import com.example.aquacube.model.User
+
+class UserAdapter(
+    var context: Context,
+    private var userList: ArrayList<User>):
+    RecyclerView.Adapter<UserAdapter.UserViewHolder>()
+{
+    inner class UserViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val binding: ItemProfileBinding = ItemProfileBinding.bind(itemView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val v = LayoutInflater.from(context).inflate(R.layout.item_profile, parent,false)
+        return UserViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val user = userList[position]
+        holder.binding.username.text = user.name
+        Glide.with(context).load(user.profileImage)
+            .placeholder(R.drawable.person_icon)
+            .into(holder.binding.profile)
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context, ChatActivity::class.java)
+            intent.putExtra("name",user.name)
+            intent.putExtra("image",user.profileImage)
+            intent.putExtra("uid",user.uid)
+            context.startActivity(intent)
+        }
+    }
+
+    override fun getItemCount(): Int = userList.size
 }
