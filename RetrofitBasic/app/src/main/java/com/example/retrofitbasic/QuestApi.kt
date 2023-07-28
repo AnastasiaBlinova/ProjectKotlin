2c5c4036efb89dec58850fdb2c68ccb2ee66c1e8
AnastasiaBlinova <nastya.com107@gmail.com>
2023-07-28 19:04:26 +0300

    commit

--- RetrofitBasic/app/src/main/java/com/example/retrofitbasic/QuestApi.kt
+++ RetrofitBasic/app/src/main/java/com/example/retrofitbasic/QuestApi.kt
@@ -2,11 +2,13 @@ package com.example.retrofitbasic
 
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Headers
 
 interface QuestApi {
 
 
         @GET("./getQuestList")
+        @Headers("Content-Type: application/json")
         fun getQuestList(): Single<QuestListResponse>
 
 }
