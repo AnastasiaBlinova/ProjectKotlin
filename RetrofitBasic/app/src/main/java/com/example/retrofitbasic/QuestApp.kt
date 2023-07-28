c33d3c9a181aa4c509ae0bfac9668dc6e8f6b1ad
AnastasiaBlinova <nastya.com107@gmail.com>
2023-07-28 17:42:44 +0300

    commit

---
+++ RetrofitBasic/app/src/main/java/com/example/retrofitbasic/QuestApp.kt
@@ -0,0 +1,4 @@
+package com.example.retrofitbasic
+
+class QuestApp {
+}
---
+++ RetrofitBasic/app/src/main/java/com/example/retrofitbasic/QuestListViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.retrofitbasic
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class QuestListViewModel(application: Application): AndroidViewModel(application) {
+
+    fun fetchQuestList(){
+
+    }
+}
