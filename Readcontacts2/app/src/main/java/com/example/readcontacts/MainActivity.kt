ef42b6d21ca428a567f99e432b917e10bd9f5cc0
AnastasiaBlinova <nastya.com107@gmail.com>
2023-08-26 12:09:30 +0300

    commit

--- Readcontacts2/app/src/main/java/com/example/readcontacts/MainActivity.kt
+++ Readcontacts2/app/src/main/java/com/example/readcontacts/MainActivity.kt
@@ -1,12 +1,47 @@
 package com.example.readcontacts
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.example.readcontacts.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val bundle =
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val  launcher = registerForActivityResult(ActivityResultContracts.RequestPermission()){ isGranted ->
+        if (isGranted){
+            getContacts()
+        } else {
+            Toast.makeText(this,"Permission is not Granted", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        checkPermissions()
+    }
+
+    private fun checkPermissions() {
+        return if (
+            ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_CONTACTS
+            ) == PackageManager.PERMISSION_GRANTED
+        ){
+            getContacts()
+        } else {
+            launcher.launch(android.Manifest.permission.READ_CONTACTS)
+        }
+    }
+
+    private fun getContacts() {
+        TODO("Not yet implemented")
     }
 }
