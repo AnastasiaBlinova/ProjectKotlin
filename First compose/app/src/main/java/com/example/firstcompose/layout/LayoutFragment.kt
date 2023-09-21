a0b3785827ab00308b480908d95e76c8500e7d9d
AnastasiaBlinova <nastya.com107@gmail.com>
2023-09-21 15:09:43 +0300

    first compose

--- First compose/app/src/main/java/com/example/firstcompose/layout/LayoutFragment.kt
+++ First compose/app/src/main/java/com/example/firstcompose/layout/LayoutFragment.kt
@@ -1,4 +1,48 @@
 package com.example.firstcompose.layout
 
-class LayoutFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.stringResource
+import androidx.fragment.app.Fragment
+import com.example.firstcompose.R
+
+class LayoutFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = ComposeView(requireContext()).apply {
+        setContent{
+            val title = stringResource(id = R.string.lorem_ipsum)
+            val description = (0..10).joinToString { title }
+
+            Body(
+                url = "https://picsum.photos/300/300",
+                title = title,
+                description = description,
+                price = 300.00,
+                comments = (0..5).map{
+                    Comment(
+                        image = "https://picsum.photos/300/300",
+                        name = "Hello",
+                        text = stringResource(id = R.string.lorem_ipsum)
+                    )
+                }
+            )
+        }
+    }
+}
+@Composable
+fun Body(
+    url: String?,
+    title: String,
+    description: String,
+    price: Double,
+    comments: List<Comment>
+){
+
 }
