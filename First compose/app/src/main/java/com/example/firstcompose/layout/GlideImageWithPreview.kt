f2cc551ac695d14838e52957c57d838fa4778203
AnastasiaBlinova <nastya.com107@gmail.com>
2023-09-21 14:55:53 +0300

    first compose

---
+++ First compose/app/src/main/java/com/example/firstcompose/layout/GlideImageWithPreview.kt
@@ -0,0 +1,33 @@
+package com.example.firstcompose.layout
+
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.example.firstcompose.R
+
+@Composable
+fun GlideImageWithPreview (
+    data: Any?,
+    modifier: Modifier? = null,
+    contentDescription: String? = null,
+    contentScale: ContentScale = ContentScale.Fit
+) {
+    if (data == null)
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = contentDescription,
+            modifier = modifier ?: Modifier,
+            alignment = Alignment.Center,
+            contentScale = contentScale
+        )
+    else
+        GlideImage(
+            data = data,
+            contentDescription = contentDescription,
+            modifier = modifier ?: Modifier,
+            contentScale = contentScale
+        )
+}
