b19f1c96cc7eb6c6c41502dd88a5fdbb120fd70a
AnastasiaBlinova <nastya.com107@gmail.com>
2023-09-21 15:40:52 +0300

    first compose

--- First compose/app/src/main/java/com/example/firstcompose/layout/LayoutFragment.kt
+++ First compose/app/src/main/java/com/example/firstcompose/layout/LayoutFragment.kt
@@ -7,13 +7,18 @@ import android.view.ViewGroup
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
@@ -79,8 +84,34 @@ fun Body(
                         .align(Alignment.CenterVertically)
                         .padding(end = 6.dp),
                     fontSize = 18.sp,
+                    maxLines = 1,
+                    textAlign = TextAlign.Start,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "$price $",
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Start,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+            Text(description, Modifier.padding(all = 20.dp))
+            
+            comments.forEach { CommentView(comment = it) }
+            
+            Button(
+                onClick = { print("oops") },
+                modifier = Modifier
+                    .padding(20.dp)
+                    .fillMaxWidth(),
+            ) {
+                Text(
+                    modifier = Modifier.padding(10.dp),
+                    text = "BUY",
+                    color = Color(0xFF000000),
                 )
 
+                
             }
         }
     }
