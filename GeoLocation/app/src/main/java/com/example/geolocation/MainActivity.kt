a5eac93b45b54f6be175d3a53530c401aa6135da
AnastasiaBlinova <nastya.com107@gmail.com>
2023-09-22 16:42:23 +0300

    Get location

--- GeoLocation/app/src/main/java/com/example/geolocation/MainActivity.kt
+++ GeoLocation/app/src/main/java/com/example/geolocation/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.example.geolocation
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.example.geolocation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val launcher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ){ map ->
+        if (map.values.isNotEmpty() && map.values.all {it}){
+            startLocation()
+        }
+    }
+
+    private fun startLocation() {
+        TODO("Not yet implemented")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        checkPermissions()
+    }
+
+    private fun checkPermissions(){
+        if (REQUIRED_PERMISSIONS.all {permission ->
+                ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+            }) {
+            startLocation()
+        } else {
+            launcher.launch(REQUIRED_PERMISSIONS)
+        }
+    }
+
+    companion object {
+        private val REQUIRED_PERMISSIONS: Array<String> = arrayOf(
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION
+        )
     }
 }
