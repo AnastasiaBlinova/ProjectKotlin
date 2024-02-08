46b384301e347d42c29572be0be78211a6c23b57
Nastya <nastya.com107@gmail.com>
2024-02-08 07:04:40 +0300

    commit

--- potoki/src/main/kotlin/Lesson_13_6/Main.kt
+++ potoki/src/main/kotlin/Lesson_13_6/Main.kt
@@ -8,19 +8,54 @@ import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
-fun main(){
-//    val scope = CoroutineScope(Dispatchers.Default)
-//    scope.launch {
-    runBlocking {
-        launch {
-            println("Start runBlocking...")
-            // doSomeWork()
-            suspendMagicNumber()
-            println("Finish runBlocking...")
+val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+    println("error handler - ${throwable.message}")
+}
+
+//val parentJob = Job()  // 1
+//val scope = CoroutineScope(parentJob + Dispatchers.Default) // 1
+//val scope = CoroutineScope( Dispatchers.Default) // 2
+val scope = CoroutineScope( SupervisorJob() + Dispatchers.Default + exceptionHandler) //3
+// SupervisorJob() 5 - в случае ошибки не отменяет все  (18)
+ suspend fun  main(){
+
+
+ //   runBlocking {
+//
+//        scope.launch {
+//            println("Start runBlocking...")
+//            // doSomeWork()
+//            suspendMagicNumber()
+//            println("Finish runBlocking...")
+//        }
+//        println("Start working..")
+     canceledCoroutine() //ошибка в одной отменяет все остальные
+     //2
+     (scope.coroutineContext.job as? CompletableJob)?.let {
+         it.complete()
+         it.join()
+     } ?: throw  IllegalStateException()
+    // parentJob.complete()    //1 переводим в состояние завершения
+    // parentJob.join()        //1 дождемся когда она перейдет в состояние - завершен
+   //     scope.coroutineContext.job.join()
+ //   }
+}
+
+//отмена всех дочерних корунтин при появлении ошибки
+//ошибка в одной отменяет все остальные
+fun canceledCoroutine(){
+    scope.launch {
+        delay(3000)
+        error("test exception")
+    }
+    scope.launch {
+        var i = 0
+        while (true){
+            delay(200)
+            println(i++)
         }
     }
 }
-
 suspend fun doSomeWork(){
     println("Start work...")
     delay(3000)
