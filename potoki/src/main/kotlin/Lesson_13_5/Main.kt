5b031a3b724c7e4010f16d222a0168b3cb309fdd
Nastya <nastya.com107@gmail.com>
2024-02-08 06:21:53 +0300

    commit

--- potoki/src/main/kotlin/Lesson_13_5/Main.kt
+++ potoki/src/main/kotlin/Lesson_13_5/Main.kt
@@ -6,18 +6,26 @@ import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking{
     launch {
-        delay(2000)
-        print("world")
+        printWorld()
     }
     launch {
-        for (i in 1..500){
-            delay(10)
-            if (i % 130 == 0)
-                println(".$i")
-            else
-                print(".$i")
-        }
+        printDots()
     }
 
     println("Hello")
+}
+
+suspend fun printWorld(){
+    delay(2000)
+    print("world")
+}
+
+suspend fun printDots (){
+    for (i in 1..500){
+        delay(10)
+        if (i % 130 == 0)
+            println(".$i")
+        else
+            print(".$i")
+    }
 }
