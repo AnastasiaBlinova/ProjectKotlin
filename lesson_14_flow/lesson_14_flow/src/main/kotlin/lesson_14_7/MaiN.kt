c789ddb7714305f42cc06970d3aece34f7eb9a41
Blinova Anastasia Valerievna <116026956+AnastasiaBlinova@users.noreply.github.com>
2024-03-21 06:55:11 +0300

    Update MaiN.kt

--- lesson_14_flow/lesson_14_flow/src/main/kotlin/lesson_14_7/MaiN.kt
+++ lesson_14_flow/lesson_14_flow/src/main/kotlin/lesson_14_7/MaiN.kt
@@ -4,10 +4,30 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.takeWhile
 import kotlin.random.Random
 
 fun main() {
+    val numbers = Numbers()
     runBlocking {
+        launch {
+            delay(2000)
+            withTimeout(10000){  // withTimeout  отписка таймаута через время
+                numbers.sharedFlow.collect {
+                    println(it)
+                }
+            }
+
+        }
+                                                          // добавим еще обного подписчика
+        launch {
+            delay(4000)
+            withTimeout(8000) {
+                numbers.sharedFlow.collect {
+                    println("second collector - $it")
+                }
+            }
+        }
 //        launch {
 //            var counter = 0                             //  1 вариант Для отмены
 //            GeneratorShared.sharedFlow.collect {
@@ -26,11 +46,30 @@ fun main() {
 //        delay(5000)
 //        job.cancel()
 
-
+//        launch {
+//            GeneratorShared.sharedFlow
+//                .takeWhile { it != 100 }
+//                .collect { println(it)                 }
+//        }
     }
 }
 
-object GeneratorShared {
+class Numbers {                                                // для демонстрации горячего источника
+    private val scope = CoroutineScope(Job() + Dispatchers.Default)
+    private val _sharedFlow = MutableSharedFlow<Int>( replay = 2)       //replay ко-во элементов, который получит новый подписчик sharedflow из буфера
+    val sharedFlow = _sharedFlow.asSharedFlow()
+
+    init    {
+        scope.launch {
+            for (i in 0..10){
+                _sharedFlow.emit(i)
+                delay(500)
+            }
+        }
+    }
+}
+
+object GeneratorShared {                                         // для демонстрации холодного источника
     private val scope = CoroutineScope(Job() + Dispatchers.Default)
     private val _sharedFlow = MutableSharedFlow<Int>()
     val sharedFlow = _sharedFlow.asSharedFlow()
@@ -41,7 +80,8 @@ object GeneratorShared {
                 _sharedFlow.emit(Random.nextInt(90,101))
                 delay(500)
             }
-
+            _sharedFlow.emit(100)
+   //         scope.cancel()                               для варианта отмены
         }
     }
 }
